35ad001a88ba47924eb5fc4dc9c9c3d180a1fa6c
n-ce <abrocks1120@gmail.com>
2024-08-02 14:05:19 +0530

    fix xml manipulation not persisting

--- api/vercel.mjs
+++ api/vercel.mjs
@@ -5,14 +5,12 @@ export default async function handler(req, res) {
 
   return fetch(`https://pipedapi.kavin.rocks/rss/playlists/${req.query.id || 'PLycDFnZLmfaDgAij1RGliVANxaqg0Ar_W'}`)
     .then(res => res.text())
-    .then(async xml => {
-      const values = xml
+    .then(xml => {
+      xml
         .split('\n')
         .filter(v => v.includes('<link>') && v.includes('watch?v='))
-        .map(v => v.slice(12, 55));
-
-      await Promise.all(
-        values.map(v =>
+        .map(v => v.slice(12, 55))
+        .forEach(v =>
           fetch('https://api.cobalt.tools/api/json', {
             method: 'POST',
             headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
@@ -24,9 +22,9 @@ export default async function handler(req, res) {
             })
           })
             .then(_ => _.json())
-            .then(data => xml.replace(v, data.url))
+            .then(data => { xml = xml.replace(v, data.url) })
         )
-      )
+
 
       return res.send(xml);
     });
