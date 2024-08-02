90880dff4ebe10184a0585e9b5f1f3feca822b06
n-ce <abrocks1120@gmail.com>
2024-08-02 14:10:29 +0530

    fix attempt

--- api/vercel.mjs
+++ api/vercel.mjs
@@ -5,25 +5,25 @@ export default async function handler(req, res) {
 
   return fetch(`https://pipedapi.kavin.rocks/rss/playlists/${req.query.id || 'PLycDFnZLmfaDgAij1RGliVANxaqg0Ar_W'}`)
     .then(res => res.text())
-    .then(xml => {
-      xml
+    .then(async xml => {
+      const values = xml
         .split('\n')
         .filter(v => v.includes('<link>') && v.includes('watch?v='))
-        .map(v => v.slice(12, 55))
-        .forEach(v =>
-          fetch('https://api.cobalt.tools/api/json', {
-            method: 'POST',
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              url: v,
-              isAudioOnly: true,
-              aFormat: 'opus',
-              filenamePattern: 'basic'
-            })
+        .map(v => v.slice(12, 55));
+
+      for await (const v of values)
+        await fetch('https://api.cobalt.tools/api/json', {
+          method: 'POST',
+          headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            url: v,
+            isAudioOnly: true,
+            aFormat: 'opus',
+            filenamePattern: 'basic'
           })
-            .then(_ => _.json())
-            .then(data => { xml = xml.replace(v, data.url) })
-        )
+        })
+          .then(_ => _.json())
+          .then(data => { xml = xml.replace(v, data.url) })
 
 
       return res.send(xml);
